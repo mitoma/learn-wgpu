d1f8a82580e24c605441ae35289194a9047986ba
mitoma <mutetheradio@gmail.com>
2020-06-20 10:53:12 +0900

    日本語ページを自分のリポジトリにデプロイするように修正。

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # abort on errors
 set -e
 
+# install libs
+npm ci
+
 # build
 npm run build
 
@@ -20,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sotrh/learn-wgpu.git master:gh-pages
+git push -f https://github.com/mitoma/learn-wgpu.git master:gh-pages
 
 cd -
